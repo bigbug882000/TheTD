0429df3686eeb2b6664a1450443909d439a5a4ac
Larry Shih <bigbug882000@gmail.com>
2015-10-20 01:36:03 +1100

    initial commit

---
+++ Assets/TDTK_Free/Scripts/ProjectileCost.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using TDTK;
+
+public class ProjectileCost : MonoBehaviour {
+
+	//public GameControl gameControl;
+	public ResourceManager resourceManager;
+	public float projectileCost;
+	
+	
+	// Use this for initialization
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null){
+			resourceManager = gameControllerObject.GetComponent<ResourceManager>();
+			
+			float  v = (float) resourceManager.rscList[0].value;
+			v += projectileCost;	//cast v as float
+			resourceManager.rscList[0].value = (int) v;
+		}
+		if (resourceManager == null){
+			Debug.Log ("Cannot fine 'GameController' Script");
+		}
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TDTK_Free/Scripts/RscRegen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using TDTK;
+
+public class RscRegen : MonoBehaviour {
+
+	//public GameControl gameControl;
+	public ResourceManager resourceManager;
+	public float rscGenRate;
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null){
+			resourceManager = gameControllerObject.GetComponent<ResourceManager>();
+
+			float r = resourceManager.rscGenRateList[0];
+			r += rscGenRate;
+			resourceManager.rscGenRateList [0] = r;
+		}
+		if (resourceManager == null){
+			Debug.Log ("Cannot fine 'GameController' Script");
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
